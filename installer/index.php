dcd8cb49e3fe14d09bfbccde720a3ec15cd0c562
Thomas Presthus <thomas@erus.net>
2007-04-28 23:14:47 +0000

    Made initial database updater / installer. Just point your webbrowser to installer/ . Database structure is maintained in tables.dat

---
+++ installer/index.php
@@ -0,0 +1,4 @@
+<p>
+By continuing this process, you confirm you awareness of any danger involved when updating the database structure. =)
+</p>
+<a href="run.php">Update / install database</a>
---
+++ installer/run.php
@@ -0,0 +1,84 @@
+<?php
+
+require("table.php");
+require('../config.php');
+require('../inc/shared_functions.php');
+
+mysql_connect($sql_host, $sql_user, $sql_pass);
+mysql_select_db($sql_base);
+
+$db_file = "tables.dat";
+
+$db_tables = array();
+$file_tables = array();
+
+$tblq = db_query("SHOW TABLES");
+while($table = mysql_fetch_array($tblq))
+{
+	$db_tables[$table[0]] = new table($table[0], true);
+}
+
+
+
+$file = file($db_file);
+$pr_tbl = null;
+$curtbl = null;
+
+foreach($file as $line)
+{
+	$line = trim($line);
+	
+	if($line{0} == '#' || empty($line))
+		continue;
+	
+	if(!$pr_tbl)
+	{
+		if(preg_match('@^TABLE ?([^/]+)@i', $line, $tblname))
+		{
+			$tblname = $tblname[1];
+			$pr_tbl = str_replace("[prefix]", $sql_prefix, $tblname);
+			$curtbl = & new table($pr_tbl);
+			$file_tables[$pr_tbl] = &$curtbl;
+			continue;
+		}
+	}
+	else
+	{
+		if($line{0} == '{')
+			continue;
+		if($line{0} == '}')
+		{
+			$pr_tbl = null;
+			continue;
+		}
+		
+		$col = preg_split("/([,]+)/", $line);
+		$curtbl->add_column(trim($col[0]), trim($col[1]), trim($col[2]), trim($col[3]), trim($col[4]), trim($col[5]));
+	}
+}
+
+foreach($file_tables as $key => $file_tbl)
+{
+	if(array_key_exists($key, $db_tables))
+	{
+		// Table exists in db
+		$db_tables[$key]->cmp($file_tbl);
+	}
+	else
+	{
+		// Table is new
+		$file_tbl->create();
+	}
+}
+
+foreach($db_tables as $key => $dbtbl)
+{
+	if(!array_key_exists($key, $file_tables))
+	{
+		db_query("DROP TABLE `$key`");
+	}
+}
+
+?>
+<h1>Finito!</h1>
+<p>Your database should now be up to date with the current codebase.</p>
