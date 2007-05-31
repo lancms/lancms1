9657bdab44809e0f7d839dbaaffa78e40f491400
Lars Åge Kamfjord <lars-github@kamfjord.org>
2007-05-31 18:08:48 +0000

    Added half-started files from my laptop. begun work on wannabe-module.

---
+++ modules/wannabe/wannabe.php
@@ -0,0 +1,11 @@
+<?php
+
+$eventID = $sessioninfo->eventID;
+$action = $_GET['action'];
+$acl_access("wannabeadmin", "", 
+
+
+if($action == "adminWannabe")
+{
+	/* Adminlist for wannabe-actions */
+	
