8da947cd26859b76a45fc44fb794be5c1bc08e66
Lars Åge Kamfjord <lars-github@kamfjord.org>
2007-12-28 16:03:15 +0000

    Fixed bug; when user registers, it is echo()'ed instead of $content .='ed

--- modules/register/register.php
+++ modules/register/register.php
@@ -11,16 +11,16 @@ if($action == "register")
 	$EMail = $_POST['EMail'];
 	$firstName = $_POST['firstName'];
 	$lastName = $_POST['lastName'];
-	
+
 	/* Check if the username is free */
-	$qCheckUsername = db_query("SELECT nick FROM ".$sql_prefix."_users 
+	$qCheckUsername = db_query("SELECT nick FROM ".$sql_prefix."_users
 		WHERE nick LIKE '".db_escape($username)."'");
 	if(db_num($qCheckUsername) != 0)
 	{
 		$register_invalid = lang("Username already in use", "register");
 		$username = FALSE;
 	} // end check if username is free
-	
+
 	/* Check if the passwords match */
 	elseif ( $pass1 != $pass2)
 	{
@@ -28,7 +28,7 @@ if($action == "register")
 		$pass1 = FALSE;
 		$pass2 = FALSE;
 	} // End check if passwords match
-	
+
 	/* Check if firstName is valid */
 	elseif(config("register_firstname_required", 0))
 	{
@@ -38,7 +38,7 @@ if($action == "register")
 			$register_invalid = lang("Firstname must be set", "register");
 		} // End if strlen firstName
 	} // End register_firstname_required
-	
+
 	/* Check if lastName is valid */
 	elseif(config("register_lastname_required", 0))
 	{
@@ -47,14 +47,14 @@ if($action == "register")
 			$lastName = FALSE;
 			$register_invalid = lang("Lastname must be set", "register");
 		} // End if strlen lastName
-		
+
 	} // End register_lastname_required
-	
-	
-	// If something went wrong in the registration; 
+
+
+	// If something went wrong in the registration;
 	// view the registration-page once more, with all fields marked.
 	// Otherwise; register the user
-	
+
 	if($register_invalid == FALSE)
 	{
 		$hide_register = TRUE;
@@ -66,15 +66,15 @@ if($action == "register")
 			firstName = '".db_escape($firstName)."',
 			lastName = '".db_escape($lastName)."'
 		");
-	
-	echo "User registered";
+
+	$content .= "User registered";
 	} // End if register_invalid = FALSE
-	
+
 } // End action = regsiter
-	
+
 if(!isset($action) || $hide_register == FALSE)
 {
-	
+
 	if($register_invalid) echo "<font color=red>$register_invalid</font><br><br>";
 
 	$content .= "<form method=POST action=?module=register&amp;action=register>\n";
@@ -90,11 +90,11 @@ if(!isset($action) || $hide_register == FALSE)
 		lang("Last name", "register");
 	$content .= "<br><input type=submit value='".lang("Create user", "register")."'>";
 	$content .= "</form>\n";
-		
+
 	// Here we should do config()-checks to see if we should ask the user about these stuff...
-	if(config("register_ask_postnumber") && config("register_postnumber_AJAX")) 
+	if(config("register_ask_postnumber") && config("register_postnumber_AJAX"))
 	{
 		$head .= "<!-- Here we can input AJAX-code for postnumbers -->";
 	} // End if AJAX postnumbers
-		
+
 } // End elseif !isset $action
