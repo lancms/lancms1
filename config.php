939015e9d3a4936668938cbb9e0eeaf5171b8ec5
Lars Åge Kamfjord <lars-github@kamfjord.org>
2008-11-07 20:51:26 +0000

    Added support for an override-file for SVN/MySQL-config, making it possible to run OSGL directly from SVN

--- config.php
+++ config.php
@@ -2,20 +2,26 @@
 
 ## DB-connection
 
-$sql_type = "mysql"; // SQL type. Valid are... mysql actually
-$sql_host = "localhost"; // SQL Host
-$sql_user = "OSGL"; // SQL username
-$sql_pass = "ComPuterParty"; // Very very secret, if you read this, you should probably go shoot yourself, just to be safe
-$sql_base = "OSGL"; // The database to use
-$sql_prefix = "osgl"; // Someone asked for this a while back. prefix, and _ is added automatically
+if(file_exists("SVN_OverrideConfig.php")) {
+    include_once 'SVN_OverrideConfig.php';
+}
+
+else {
+
+    $sql_type = "mysql"; // SQL type. Valid are... mysql actually
+    $sql_host = "localhost"; // SQL Host
+    $sql_user = "OSGL"; // SQL username
+    $sql_pass = "ComPuterParty"; // Very very secret, if you read this, you should probably go shoot yourself, just to be safe
+    $sql_base = "OSGL"; // The database to use
+    $sql_prefix = "osgl"; // Someone asked for this a while back. prefix, and _ is added automatically
 
 
 
-$osgl_session_cookie = "OSGL2-cake";
-$language = "norwegian"; // The user might want to customize this him self... Might be a FIXME
+    $osgl_session_cookie = "OSGL2-cake";
+    $language = "norwegian"; // The user might want to customize this him self... Might be a FIXME
 
-$design_title = "OSGlobeLAN";
-
+    $design_title = "OSGlobeLAN";
+}
 ## All other settings should be done in some kind of installer....
 
 
