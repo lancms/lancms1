3e261defcc8c4d3b999c753b696a165b0ca9db65
Lars Åge Kamfjord <lars-github@kamfjord.org>
2008-11-07 21:54:08 +0000

    Added some admin-ing of events for global admins. List events, set public/private

--- modules/globaladmin/globaladmin.php
+++ modules/globaladmin/globaladmin.php
@@ -6,6 +6,31 @@ if(acl_access("globaladmin", "", 0) != "Admin") die("You do not have propper rig
 
 if(!isset($action))
 {
+	$content .= "<table>";
+	$content .= "<tr><th>".lang("Event name", "globaladmin");
+	$content .= "</th><th>";
+	$content .= lang("Admin this event", "globaladmin");
+	$content .= "</th><th>";
+	$content .= "</th><th>".lang("Set event public", "globaladmin");
+	$content .= "</th></tr>\n\n";
+
+	$qListEvents = db_query("SELECT * FROM ".$sql_prefix."_events WHERE ID != 1");
+	while($rListEvents = db_fetch($qListEvents)) {
+	    $content .= "<tr><td>";
+	    $content .= $rListEvents->eventname;
+	    $content .= "</td><td>";
+	    $content .= "<a href=?module=events&amp;action=setCurrentEvent&amp;eventID=$rListEvents->ID>";
+	    $content .= lang("Set active", "globaladmin");
+	    $content .= "</a>";
+	    $content .= "</td><td>";
+	    if($rListEvents->eventPublic == 1) {
+	        $content .= "<a href=?module=globaladmin&amp;eventID=$rListEvents->ID&amp;action=setPrivate>".lang("Public", "globaladmin")."</a>";
+	    } else {
+	        $content .= "<a href=?module=globaladmin&amp;eventID=$rListEvents->ID&amp;action=setPublic>".lang("Private", "globaladmin")."</a>";
+	    }
+	    $content .= "</td></tr>\n\n\n";
+	} // End while(rListEvents)
+	$content .= "</table>";
 	/* List of global admin-options */
 	$content .= "<br><a href=?module=globaladmin&amp;action=addEvent>".lang("Add new event", "globaladmin")."</a>";
 	
@@ -56,3 +81,13 @@ elseif($action == "doAddEvent")
 	
 	
 } // End if action = doAddEvent
+
+elseif($action == "setPrivate") {
+    db_query("UPDATE ".$sql_prefix."_events SET eventPublic = 0 WHERE ID = '".db_escape($_GET['eventID'])."'");
+    header("Location: ?module=globaladmin");
+}
+
+elseif($action == "setPublic") {
+    db_query("UPDATE ".$sql_prefix."_events SET eventPublic = 1 WHERE ID = '".db_escape($_GET['eventID'])."'");
+    header("Location: ?module=globaladmin");
+}
