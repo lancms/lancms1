19387d02c14e126bf797b62fbed73157807c10b3
Lars Åge Kamfjord <lars-github@kamfjord.org>
2008-12-12 21:55:14 +0000

    Added goto-code in change eventID-code. Fixed goto after creating an event

--- modules/events/events.php
+++ modules/events/events.php
@@ -7,14 +7,17 @@ $eventID = $_GET['eventID'];
 if($action == "setCurrentEvent" && isset($eventID))
 {
 	$escape = db_escape($eventID); // Prepare it for SQL
-	
+
 	// Update the session-table
-	db_query("UPDATE ".$sql_prefix."_session SET 
-		eventID = '$escape' 
+	db_query("UPDATE ".$sql_prefix."_session SET
+		eventID = '$escape'
 		WHERE sID = '".$_COOKIE[$osgl_session_cookie]."'");
-		
+
+	// Goto $_GET['gotomodule'] if exists
+	if(!empty($_GET['gotomodule']))
+		header("Location: index.php?module=".$_GET['gotomodule']);
 	// Return to referer if it exists
-	if(!empty($_SERVER['HTTP_REFERER']))
+	elseif(!empty($_SERVER['HTTP_REFERER']))
 		header("Location: ".$_SERVER['HTTP_REFERER']);
 	else // If no referer, go back to index. Should probably never happen...
 		header("Location: index.php");
--- modules/globaladmin/globaladmin.php
+++ modules/globaladmin/globaladmin.php
@@ -33,37 +33,37 @@ if(!isset($action))
 	$content .= "</table>";
 	/* List of global admin-options */
 	$content .= "<br><a href=?module=globaladmin&amp;action=addEvent>".lang("Add new event", "globaladmin")."</a>";
-	
+
 } // End if !isset($action)
 
 
 elseif($action == "addEvent")
 {
 	/* Action to add new events */
-	
+
 	if(isset($_GET['errormsg'])) $content .= $_GET['errormsg']."<br>\n";
-	
+
 	$content .= "<form method=POST action=?module=globaladmin&amp;action=doAddEvent>\n";
 	$content .= "<input type=text name=eventname value='".$_GET['eventname']."'>\n".lang("Name of event", "globaladmin");
 	$content .= "<br><input type=submit value='".lang("Add event", "globaladmin")."'>";
-	
-	
-	
+
+
+
 } // End action = addEvent
 
 
 elseif($action == "doAddEvent")
 {
 	$eventname = $_POST['eventname'];
-	
+
 	$qCheckExistingName = db_query("SELECT COUNT(eventname) AS count FROM ".$sql_prefix."_events WHERE eventname LIKE '".db_escape($eventname)."'");
 	$rCheckExistingName = db_fetch($qCheckExistingName);
-	
+
 	if($rCheckExistingName->count > 0)
 	{
 		header("Location: ?module=globaladmin&action=addEvent&eventname=$eventname&errormsg=Event already exists");
 	} // End if count of existing name > 0, make error
-	
+
 	else
 	{
 		// Name doesn't exists already; create it
@@ -74,12 +74,12 @@ elseif($action == "doAddEvent")
 			");
 		$qCheckEventID = db_query("SELECT ID FROM ".$sql_prefix."_events WHERE eventname LIKE '".db_escape($eventname)."'");
 		$rCheckEventID = db_fetch($qCheckEventID);
-		
-		header("Location: ?module=eventadmin&eventID=$rCheckEventID->ID");
+
+		header("Location: ?module=events&action=setCurrentEvent&gotomodule=eventadmin&eventID=".$rCheckEventID->ID);
 	} // End else (if name doesn't exists; create it)
-			
-	
-	
+
+
+
 } // End if action = doAddEvent
 
 elseif($action == "setPrivate") {
