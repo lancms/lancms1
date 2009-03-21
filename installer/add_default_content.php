5d1a8175ec235630c34332dcbe815ef3df110cfb
Lars Åge Kamfjord <lars-github@kamfjord.org>
2009-03-21 22:25:22 +0000

    added a default globaladmin with password admin

--- installer/add_default_content.php
+++ installer/add_default_content.php
@@ -1,11 +1,16 @@
 <?php
 
 
-if(db_num(db_query("SELECT * FROM ".$sql_prefix."_users WHERE ID = 1")) == 0) 
+if(db_num(db_query("SELECT * FROM ".$sql_prefix."_users WHERE ID = 1")) == 0)
 	db_query("INSERT INTO ".$sql_prefix."_users SET ID = 1, nick = 'Anonymous user'");
 
-if(db_num(db_query("SELECT * FROM ".$sql_prefix."_events WHERE ID = 1")) == 0) 
+if(db_num(db_query("SELECT * FROM ".$sql_prefix."_events WHERE ID = 1")) == 0)
 	db_query("INSERT INTO ".$sql_prefix."_events SET ID = 1, eventname = 'Ingen event'");
 
 if(db_num(db_query("SELECT * FROM ".$sql_prefix."_groups WHERE ID = 1")) == 0)
 	db_query("INSERT INTO ".$sql_prefix."_groups SET ID = 1, groupname = '[PUBLIC]'");
+
+# Add a global admin with password "admin"
+if(db_num(db_query("SELECT * FROM ".$sql_prefix."_users WHERE ID = 2")) == 0)
+	db_query("INSERT INTO ".$sql_prefix."_users SET ID = 2, globaladmin = 1, nick = 'globaladmin', password = '".md5("admin")."'");
+
