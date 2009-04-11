d701ff69c9a353bf4a33ba2350129614a802e260
Lars Åge Kamfjord <lars-github@kamfjord.org>
2009-04-11 13:57:17 +0000

    Changed [PUBLIC]-group to grouptype access from grouptype clan

--- installer/add_default_content.php
+++ installer/add_default_content.php
@@ -8,7 +8,7 @@ if(db_num(db_query("SELECT * FROM ".$sql_prefix."_events WHERE ID = 1")) == 0)
 	db_query("INSERT INTO ".$sql_prefix."_events SET ID = 1, eventname = 'Ingen event'");
 
 if(db_num(db_query("SELECT * FROM ".$sql_prefix."_groups WHERE ID = 1")) == 0)
-	db_query("INSERT INTO ".$sql_prefix."_groups SET ID = 1, groupname = '[PUBLIC]'");
+	db_query("INSERT INTO ".$sql_prefix."_groups SET ID = 1, groupname = '[PUBLIC]', groupType = 'access'");
 
 # Add a global admin with password "admin"
 if(db_num(db_query("SELECT * FROM ".$sql_prefix."_users WHERE ID = 2")) == 0)
