87da6611011ac9baff56efa6e95b3efa8a3051ec
Lars Åge Kamfjord <lars-github@kamfjord.org>
2009-05-08 17:22:46 +0000

    List what groups a user is a member of in crewlist

--- modules/crewlist/crewlist.php
+++ modules/crewlist/crewlist.php
@@ -11,6 +11,17 @@ if(empty($action)) {
 		WHERE acl.accessmodule = 'crewlist' AND acl.access != 'No'");
 
 	$content .= '<table>';
+	$content .= "<tr><th>";
+	$content .= lang("Name", "crewlist");
+	$content .= "</th><th>";
+	$content .= lang("Nick", "crewlist");
+	$content .= "</th><th>";
+	$content .= lang("EMail", "crewlist");
+	$content .= "</th><th>";
+	$content .= lang("Cellphone", "crewlist");
+	$content .= "</th><th>";
+	$content .= lang("Access", "crewlist");
+	$content .= "</th></tr>";
 	while($rGetCrewMembers = db_fetch($qGetCrewMembers)) {
 		$qCrewinfo = db_query("SELECT * FROM ".$sql_prefix."_users WHERE ID = '$rGetCrewMembers->userID'");
 		$rCrewinfo = db_fetch($qCrewinfo);
@@ -22,6 +33,12 @@ if(empty($action)) {
 		$content .= $rCrewinfo->EMail;
 		$content .= "</td><td>";
 		$content .= $rCrewinfo->cellphone;
+		$content .= "</td><td>";
+		$qListCrews = db_query("SELECT groupname FROM ".$sql_prefix."_groups JOIN ".$sql_prefix."_group_members  
+			ON ".$sql_prefix."_groups.ID=".$sql_prefix."_group_members.groupID WHERE ".$sql_prefix."_group_members.userID = '$rGetCrewMembers->userID' AND eventID = '$sessioninfo->eventID' AND groupType = 'access'");
+		while($rListCrews = db_fetch($qListCrews)) {
+			$content .= $rListCrews->groupname." ";
+		} // End while
 		$content .= "</td></tr>";
 	} // End while
 	$content .= "</table>";
