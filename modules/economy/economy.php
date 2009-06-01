0387571a1c7928f37273ab25e51a6c5549c1d5cc
Lars Åge Kamfjord <lars-github@kamfjord.org>
2009-06-01 20:56:08 +0000

    Added some more functionality to economy (but nothing working yet)

--- modules/economy/economy.php
+++ modules/economy/economy.php
@@ -10,6 +10,7 @@ if($acl_access == 'No') die("You do not have access to economy!");
 $acl = acl_access("economy", "", $sessioninfo->eventID);
 
 if(empty($action) || $action == "editAccount") {
+	$content .= economy_display_submenu();
 	$qGetAccounts = db_query("SELECT * FROM ".$sql_prefix."_economy_accounts
 		WHERE eventID = $sessioninfo->eventID ORDER BY accountnumber ASC");
 	$content .= '<table>';
@@ -106,3 +107,82 @@ elseif($action == "doEditAccount" && $acl == 'Admin' && !empty($account)) {
 		AND accountnumber = '".db_escape($account)."'");
 	header("Location: ?module=economy");
 } // End elseif action == doEditAccount
+
+elseif($action == "receipt" && $acl == 'Admin') {
+	$content .= economy_display_submenu();
+	$content .= "<table>\n";
+	$qListReceipt = db_query("SELECT * FROM ".$sql_prefix."_economy_receipt WHERE eventID = '$sessioninfo->eventID'");
+
+	while($rListReceipt = db_fetch($qListReceipt)) {
+		$content .= "<tr><td>";
+		$content .= $rListReceipt->ID;
+		$content .= "</td><td>";
+		$content .= $rListReceipt->from_location;
+		$content .= "</td></tr>\n";
+	} // End while
+	$content .= "</table>\n\n";
+
+	$content .= "<form method=POST action=?module=economy&action=addReceipt>\n";
+	$content .= "<input type=text name='from_location'> ".lang("From", "economy");
+	$content .= "<br />";
+	$content .= "<input type=text name=timestamp> ".lang("Timestamp (YYYY-MM-DD HH:MM)", "economy");
+	$content .= "<br /><input type=text name=description> ".lang("Description", "economy")."\n";
+	$content .= "<br /><input type=text name=amount> ".lang("Amount", "economy")."\n";
+	$content .= "<br /><input type=submit value='".lang("Add receipt", "economy")."'>";
+	$content .= "</form>\n";
+} // End action == receipt
+
+elseif($action == "addReceipt" && $acl == 'Admin') {
+	$from_location = $_POST['from_location'];
+	$timestamp = $_POST['timestamp'];
+	$description = $_POST['description'];
+	$amount = $_POST['amount'];
+
+	db_query("INSERT INTO ".$sql_prefix."_economy_receipt 
+		SET from_location = '".db_escape($from_location)."',
+		timestamp = '".db_escape($timestamp)."',
+		description = '".db_escape($description)."',
+		amount = '".db_escape($amount)."',
+		eventID = '$sessioninfo->eventID'");
+	header("Location: ?module=economy&action=receipt");
+}
+elseif($action == "accounts" && $acl == 'Admin') {
+	$content .= economy_display_submenu();	
+
+}
+
+elseif($action == "bookkeeping" && $acl == 'Admin') {
+	$content .= economy_display_submenu();
+
+}
+function economy_display_submenu() {
+	$action = $_GET['action'];
+	$acl = acl_access("economy", "", $sessioninfo->userID);
+
+	if(empty($action) && $acl == 'Admin') {
+		$return = lang("Main", "economy");
+	} else {
+		$return = "<a href=?module=economy>".lang("Main", "economy")."</a>";
+	}
+
+	$return .= " ";
+	if($action == "receipt" && $acl == 'Admin') {
+		$return .= lang("Receipts", "economy");
+	}
+	else {
+		$return .= "<a href=?module=economy&action=receipt>".lang("Receipts", "economy")."</a>";
+	}
+	$return .= " ";
+	if($action == "bookkeeping" && $acl == 'Admin') {
+		$return .= lang("Bookkeeping", "economy");
+	} else {
+		$return .= "<a href=?module=economy&action=bookkeeping>".lang("Bookkeeping", "economy")."</a>";
+	}
+	$return .= " ";
+	if($action == "accounts" && $acl == 'Admin') {
+		$return .= lang("Accounts", "economy");
+	} else {
+		$return .= "<a href=?module=economy&action=accounts>".lang("Accounts", "economy")."</a>";
+	}
+	return $return;
+}
