01217f53be33b80b318848ff6bffa618a11ae2fe
Lars Åge Kamfjord <lars-github@kamfjord.org>
2009-10-02 20:30:10 +0000

    some minor fixes to SMS

--- inc/SMS/SMS4you.no.php
+++ inc/SMS/SMS4you.no.php
@@ -1,13 +1,13 @@
 <?php
 
 
-$qFindJobs = db_query("SELECT * FROM ".$sql_prefix."_cronjobs WHERE cronModule = 'SMS' LIMIT 0,1");
+$qFindJobs = db_query("SELECT * FROM ".$sql_prefix."_cronjobs WHERE cronModule = 'SMS' AND (finishTime < 1 OR finishTime IS NULL) LIMIT 0,20");
 
 while($rFindJobs = db_fetch($qFindJobs)) {
 
 
-	$URL = "http://gate1.sms4you.no/kundesenter/sendsms.php?user=".$SMS_user."&pass=".$SMS_pass."&fromAlpha=".$SMS_from."&destination=".$rFindJobs->toUser."&message=".urlencode($rFindJobs->content);
+	$URL = "http://gate1.sms4you.no/kundesenter/sendsms.php?user=".$SMS_user."&pass=".$SMS_pass."&fromAlpha=".$SMS_from."&destination=".$rFindJobs->toUser."&message=".$rFindJobs->content;
 #	echo $URL;
-	$fp = fopen($URL, "r");
+	$fp = fopen("$URL", "r");
 	db_query("UPDATE ".$sql_prefix."_cronjobs SET finishTime = '".time()."' WHERE jobID = '$rFindJobs->jobID'");
 }
