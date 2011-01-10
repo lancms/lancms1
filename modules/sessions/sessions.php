dd3361d132075477a344b84c78341e0bf16f15be
Mathias Bøhn Grytemark <mathias@grytemark.no>
2011-01-10 14:47:23 +0000

    headings and some styling

--- modules/sessions/sessions.php
+++ modules/sessions/sessions.php
@@ -7,10 +7,20 @@ $action = $_GET['action'];
 
 if(empty($action)) {
 
-	$content .= "<table>";
+	$content .= "<h2>".lang ("Sessions", "sessions")."</h2>";
+	$content .= "<table>\n";
+	$content .= "<tr>\n";
+	$content .= "<th>".lang ("IP-address", "sessions")."</th>\n";
+	$content .= "<th>".lang ("Useragent", "sessions")."</th>\n";
+	$content .= "<th>".lang ("User", "sessions")."</th>\n";
+	$content .= "<th>".lang ("Event", "sessions")."</th>\n";
+	$content .= "<th>".lang ("Last visit", "sessions")."</th>\n";
+	$content .= "</tr>\n";
 	$qFindSessions = db_query("SELECT * FROM ".$sql_prefix."_session");
+
+	$count = 1;
 	while($rFindSessions = db_fetch($qFindSessions)) {
-		$content .= "<tr><td>\n";
+		$content .= "<tr class='row$count'><td>\n";
 		$content .= $rFindSessions->userIP;
 		$content .= "</td><td>\n";
 		$content .= $rFindSessions->userAgent;
@@ -21,7 +31,16 @@ if(empty($action)) {
 		$content .= "</td><td>\n";
 		$content .= date("Y-m-d G:m:s", $rFindSessions->lastVisit);
 		$content .= "</td></tr>\n\n";
+
+		if ($count == 2)
+		{
+			$count = 1;
+		}
+		else
+		{
+			$count = 2;
+		}
 	} // End while
-	$content .= "</table>";
+	$content .= "</table>\n";
 
 }
