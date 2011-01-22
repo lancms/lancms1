8fcbf1696f587c51ba4391554a4fed9eb180f35c
Lars Åge Kamfjord <lars-github@kamfjord.org>
2011-01-22 16:44:02 +0100

    Updating translation-files and update-script

--- i18n/update_messages.sh
+++ i18n/update_messages.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 xgettext -Lphp -i ../*.php -i ../modules/*/*.php -i ../inc/*.php -i ../installer/*.php -klang -o messages.pot
+
+
+msgfmt -cv -o nb_NO.utf8/LC_MESSAGES/messages.mo nb.po
