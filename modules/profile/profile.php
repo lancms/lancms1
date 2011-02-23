94b34b23432a95c614054c376f2ab7cbc5cda84f
Mathias Bøhn Grytemark <mathias@grytemark.no>
2011-02-23 21:12:09 +0100

    use th instead of td for headers. use gettexts _() instead of lang(). don't show groupmembership-header if there are no groupmemberships.

--- modules/profile/profile.php
+++ modules/profile/profile.php
@@ -8,27 +8,30 @@ $rFindUser = db_fetch($qFindUser);
 
 
 $content .= "<table>";
-$content .= "<tr><td>";
-$content .= lang("Name", "profile");
-$content .= "</td><td>";
+$content .= "<tr><th>";
+$content .= _("Name");
+$content .= "</th><td>";
 $content .= $rFindUser->firstName." ".$rFindUser->lastName;
 $content .= "</td></tr>";
 
-$content .= "<tr><td>";
-$content .= lang("Nick", "profile");
-$content .= "</td><td>";
+$content .= "<tr><th>";
+$content .= _('Nick');
+$content .= "</th><td>";
 $content .= $rFindUser->nick;
 $content .= "</td></tr>";
 
-$content .= "<tr><td>";
-$content .= lang("Groupmemberships", "profile");
-$content .= "</td><td><ul>";
 $qFindGroups = db_query("SELECT g.groupname,e.eventname FROM (".$sql_prefix."_groups g JOIN ".$sql_prefix."_group_members gm ON gm.groupID=g.ID) JOIN ".$sql_prefix."_events e ON g.eventID=e.ID WHERE gm.userID = '".db_escape($user)."'");
-while($rFindGroups = db_fetch($qFindGroups)) {
-	$content .= "<li>".$rFindGroups->eventname." &ndash; ".$rFindGroups->groupname."</li>\n";
-}
-$content .= "</ul></td></tr>\n\n\n";
 
+if (mysql_num_rows($qFindGroups))
+{
+	$content .= "<tr><th>";
+	$content .= _("Groupmemberships");
+	$content .= "</th><td><ul>";
+	while($rFindGroups = db_fetch($qFindGroups)) {
+		$content .= "<li>".$rFindGroups->eventname." &ndash; ".$rFindGroups->groupname."</li>\n";
+	}
+	$content .= "</ul></td></tr>\n\n\n";
+}
 
 $content .= "</table>";
 
@@ -36,6 +39,6 @@ $content .= "</table>";
 $useradminread = acl_access ("userAdmin", "", 1);
 if ($useradminread != "No")
 {
-	$content .= "<a href='index.php?module=edituserinfo&action=editUserinfo&user=".$rFindUser->ID."'>"._("Edit userinfo")."</a>";
+	$content .= "<br /><a href='index.php?module=edituserinfo&action=editUserinfo&user=".$rFindUser->ID."'>"._("Edit userinfo")."</a>";
 }
 
