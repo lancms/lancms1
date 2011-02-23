dcd76434028885bdfa492b10f00a31ecd326e099
Mathias Bøhn Grytemark <mathias@grytemark.no>
2011-02-23 21:39:49 +0100

    fixes LP#723922. shows character counter when writing SMS

--- modules/SMS/SMS.php
+++ modules/SMS/SMS.php
@@ -8,20 +8,33 @@ $action = $_GET['action'];
 
 if(empty($action)) {
 
+	$design_head .= "<script type='text/javascript'>
+			  function checkLength(from, where) {
+						 var len = document.getElementById(from).value.length;
+						 document.getElementById(where).innerHTML = len;
+							}  
+						</script>
+						 ";
+
 	$content .= "<table>";
-	$content .= "<form method=POST action=?module=SMS&action=sendSMS>\n";
+	$content .= "<form method='POST' action='?module=SMS&action=sendSMS'>\n";
 	$content .= "<tr><td>";
-	$content .= "<select name=toSmsList>";
+	$content .= "<select name='toSmsList'>";
 	for($i=0;$i<count($smsList);$i++) {
 		$content .= "<option value='$i'>".$smsList[$i]['name']."</option>\n";
 	}
 	$content .= "</select></td></tr>";
 	$content .= "<tr><td>";
-	$content .= "<textarea name=message></textarea>";
+	$content .= "<textarea onkeyup='checkLength(\"message\", \"count\");' onkeydown='checkLength(\"message\", \"count\");' id='message' name='message'></textarea>";
 
 	$content .= "</td></tr><tr><td>";
-	$content .= "<input type=submit value='".lang("Send SMS", "SMS")."'>";
+	$content .= "<input type='submit' value='".lang("Send SMS", "SMS")."'>";
 	$content .= "</td></tr>";
+
+	$content .= "<tr><th>";
+	$content .= _("Number of characters entered:")." ";
+	$content .= "<span id='count'>0</span>";
+	$content .= "</th></tr>";
 
 	$content .= "</form></table>";
 } // End action
