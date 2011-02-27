e7aa6e91411aefbd73b7dff37ab971b24d952f08
Mathias Bøhn Grytemark <mathias@grytemark.no>
2011-02-27 19:38:10 +0100

    making some preparations in infoscreens

--- modules/infoscreens/infoscreens.php
+++ modules/infoscreens/infoscreens.php
@@ -6,10 +6,16 @@ if($acl == 'No' || $acl == 'Read') die("No access to infoscreens");
 $action = $_GET['action'];
 
 if(empty($action)) {
+
+
+#### START - screens ####
+	$content .= "<div style='border: solid 1px black; border-collapse: collapse;'>\n";
+	$content .= "<h3>"._('Screens')."</h3>";
+
 	$qFindScreens = db_query("SELECT * FROM ".$sql_prefix."_infoscreens WHERE eventID = '$sessioninfo->eventID'");
-	$content .= "<table>";
+	$content .= "<table style='border: solid 1px black; border-collapse: collapse;'>";
 	while($rFindScreens = db_fetch($qFindScreens)) {
-		$content .= "<tr><td>";
+		$content .= "<tr><td style='border: solid 1px black; border-collapse: collapse;'>";
 		$content .= $rFindScreens->name;
 		$content .= "</td></tr>\n\n";
 	}
@@ -20,8 +26,28 @@ if(empty($action)) {
 		$content .= "<form method=POST action='?module=infoscreens&action=addScreen'>\n";
 		$content .= "<input type=text name='name'>"._("Screen name");
 		$content .= "<br /><input type=submit value='"._("Add screen")."'>";
-		$content .= "</form>";
+		$content .= "</form>\n";
 	}
+
+	$content .= "</div>\n";
+#### END - screens ####
+
+#### START - slides ####
+	$content .= "<br /><div style='border: solid 1px black; border-collapse: collapse;'>\n";
+	$content .= "<h3>"._('Slides')."</h3>\n";
+	$content .= "<a href='?modules=infoscreens&action=newSlide'>"._('Create new slide')."</a>";
+	
+	# TODO: list slides here.
+	$content .= "</div>";
+#### END - slides
+	
+#### START - queues ####
+	$content .= "<br /><div style='border: solid 1px black; border-collapse: collapse;'>\n";
+	$content .= "<h3>"._('Queues')."</h3>\n";
+
+	$content .= "</div>";
+#### END - queues ####
+
 
 }
 
