e6d991b1630aac27b37f31658b6c9aa4db8089f5
Lars Åge Kamfjord <lars-github@kamfjord.org>
2011-09-18 17:49:47 +0200

    Fixing UTF-8 problems with SMS4you

--- inc/SMS/SMS4you.no.php
+++ inc/SMS/SMS4you.no.php
@@ -6,6 +6,7 @@ $qFindJobs = db_query("SELECT * FROM ".$sql_prefix."_cronjobs WHERE cronModule =
 while($rFindJobs = db_fetch($qFindJobs)) {
 
 	$message = str_replace(" ", "%20", $rFindJobs->content);
+	$message = utf8_decode($message);
 	$URL = "http://gate1.sms4you.no/kundesenter/sendsms.php?user=".$SMS_user."&pass=".$SMS_pass."&fromAlpha=".$SMS_from."&destination=".$rFindJobs->toUser."&message=".$message;
 #	echo $URL;
 	$fp = fopen("$URL", "r");
