949c14bd8a1fa61bb5d68751b612ce4e284cce38
Lars Åge Kamfjord <lars-github@kamfjord.org>
2012-04-08 21:32:04 +0200

    Updating Facebook-login code to the current at FBDeveloper

--- index.php
+++ index.php
@@ -114,18 +114,25 @@ if($sessioninfo->userID == 1)
 
 	if($facebook_appID > 0 && $facebook_login == TRUE) {
 		// Facebook Connect is set up and enabled. Using for login
-		$design_userinfo .= '<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId='.$facebook_appID.'&amp;amp;xfbml=1"></script><fb:login-button show-faces="false" width="100" max-rows="1"></fb:login-button>
-';
-
-		$design_userinfo .= "<div id=\"fb-root\"></div>
-	      <script src=\"http://connect.facebook.net/en_US/all.js\"></script>
-	      <script>
-	         FB.init({ 
-        	    appId:'$facebook_appID', cookie:true, 
-	            status:true, xfbml:true 
-        	 });
-	      </script>
-		";
+		$design_userinfo .= "<script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '".$facebook_appID."',
+            status     : true, 
+            cookie     : true,
+            xfbml      : true,
+            oauth      : true,
+          });
+        };
+        (function(d){
+           var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+           js = d.createElement('script'); js.id = id; js.async = true;
+           js.src = \"//connect.facebook.net/en_US/all.js\";
+           d.getElementsByTagName('head')[0].appendChild(js);
+         }(document));
+      </script>
+      <div class=\"fb-login-button\">Login with Facebook</div>
+	";
 
 	} // End facebook
 } // End if sessioninfo says not logged in
