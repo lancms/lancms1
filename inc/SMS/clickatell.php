ec0be14f4247ce383f9a0d312b56019734bff4d8
Edvin Hultberg <edvin@coretrek.no>
2016-11-26 09:09:56 +0100

    Add '47' to the numbers

--- inc/SMS/clickatell.php
+++ inc/SMS/clickatell.php
@@ -6,14 +6,29 @@ $rFindJobs = db_fetch_all($qFindJobs);
 
 if ((is_array($rFindJobs)) && (count($rFindJobs) > 0)) {
     foreach ($rFindJobs as $job) {
+        
+        $sendToTelephones = explode(',', $job->toUser);
+        
+        // We must append 47 to all phone numbers, clickatell requires this.
+        $sendToTelephones = array_map($sendToTelephones, function($number) {
+            return '47' . $number;
+        });
+        
         $message = str_replace(" ", "%20", $job->content);
         $message = utf8_decode($message);
         $URL = sprintf(
-            'http://api.clickatell.com/http/sendmsg?user=%s&password=%s&api_id=3600328&to=%s&text=%s',
-    		$SMS_user, $SMS_pass, $job->toUser, $message
+            'https://api.clickatell.com/http/sendmsg?user=%s&password=%s&api_id=3600328&to=%s&text=%s',
+    		$SMS_user,
+            $SMS_pass,
+            urlencode(implode(',', $sendToTelephones)),
+            urlencode($message)
         );
         
-        $fp = fopen("$URL", "r");
+        $curl = curl_init($URL);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_exec($curl);
+        curl_close($curl);
+        
     	db_query(
             sprintf(
                 "UPDATE %s_cronjobs SET finishTime = '%s' WHERE jobID = '%s'",
