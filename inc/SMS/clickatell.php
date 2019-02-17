640508ee79d28c9b15c7f0ba6c526f2e7d01de12
Edvin Hultberg <hultberg@mittnett.net>
2019-02-17 21:36:29 +0100

    Fix country codes in clickatell

--- inc/SMS/clickatell.php
+++ inc/SMS/clickatell.php
@@ -6,28 +6,47 @@ $rFindJobs = db_fetch_all($qFindJobs);
 
 if ((is_array($rFindJobs)) && (count($rFindJobs) > 0)) {
     foreach ($rFindJobs as $job) {
-        
+
         $sendToTelephones = explode(',', $job->toUser);
-        
-        // We must append 47 to all phone numbers, clickatell requires this.
-        $sendToTelephones = map_array($sendToTelephones, function($number) {
-            return '47' . $number;
-        });
-        
+        $phoneNumbers = array();
+
+        foreach ($phoneNumbers as $number) {
+            $numberLength = strlen($number);
+
+            switch (strlen($number)) {
+                case 8:
+                    $phoneNumbers[] = '47' . $number;
+                    break;
+
+                case 10:
+                    $phoneNumbers[] = $number;
+                    break;
+
+                case 11:
+                    if ($number[0] === '+') {
+                        $phoneNumbers[] = substr($number, 1);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
         $message = utf8_decode($job->content);
         $URL = sprintf(
             'https://api.clickatell.com/http/sendmsg?user=%s&password=%s&api_id=3600328&to=%s&text=%s',
     		$SMS_user,
             $SMS_pass,
-            urlencode(implode(',', $sendToTelephones)),
+            urlencode(implode(',', $phoneNumbers)),
             urlencode($message)
         );
-        
+
         $curl = curl_init($URL);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_exec($curl);
         curl_close($curl);
-        
+
     	db_query(
             sprintf(
                 "UPDATE %s_cronjobs SET finishTime = '%s' WHERE jobID = '%s'",
